3e392568343c0f6d38fef9c8f10b8345e27d0b2d
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-05-09 10:24:57 +0200

    validering i Region

--- HotelsApi/HotelsApi/Domain/Region.cs
+++ HotelsApi/HotelsApi/Domain/Region.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace HotelsApi.Domain
 {
     public class Region
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int Id { get; set; }
     }
 }
