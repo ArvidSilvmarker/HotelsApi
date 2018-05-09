c92c542f5fc70218cf498bc8bbb25a197e897dbb
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-05-09 09:34:08 +0200

    Lagt till EF. Skapat context.

---
+++ HotelsApi/HotelsApi/Infrastructure/HotelContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class HotelContext : DbContext
+{
+    public DbSet<Region> Region { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server = (localdb)\\mssqllocaldb; Database = db-hotels; Trusted_Connection = True; ");
+    }
+}
