792009f992c40658946fbfaa36a3cb1a35027421
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-05-09 09:50:30 +0200

    Började på repo

---
+++ HotelsApi/HotelsApi/Domain/Interfaces/IRegionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelsApi.Domain.Interfaces
+{
+    interface IRegionRepository
+    {
+        void CreateRegion(Region region);
+        void RemoveRegion(Region region);
+        List<Region> ReadAllRegions();
+        void ReSeedRegions();
+    }
+}
