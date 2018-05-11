0a03c947f2991d2c916e6825d8cd821805823c50
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-05-11 14:45:13 +0200

    fil och filläsarinterface

---
+++ HotelsApi/HotelsApi/Domain/Interfaces/IFileReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelsApi.Domain.Interfaces
+{
+    public interface IFileReader
+    {
+        List<Hotel> ReadScandicFile();
+    }
+}
