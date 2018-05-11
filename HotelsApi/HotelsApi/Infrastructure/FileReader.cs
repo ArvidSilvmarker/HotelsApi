057d46fe859ad5e9f97687983ba91d6490f6674c
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-05-11 15:22:58 +0200

    Började på filereader

---
+++ HotelsApi/HotelsApi/Infrastructure/FileReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelsApi.Infrastructure
+{
+    public class FileReader
+    {
+        public List<Hotel> ReadScandicFile()
+        {
+            var hotelList = new List<Hotel>();
+            string[] lines = System.IO.File.ReadAllLines($@"Scandic\Scanic-{DateTime.Now:yyyy-MM-dd}.txt");
+            foreach (string line in lines)
+            {
+                var hotelString = line.Split(',');
+                hotelList.Add(new Hotel
+                {
+                });
+
+            }
+        }
+    }
+}
