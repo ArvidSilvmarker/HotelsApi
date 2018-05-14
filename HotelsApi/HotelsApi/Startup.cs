d97c1db7d1dfff21b1bc6f47365c9c77adbc7c11
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-05-14 10:28:31 +0200

    tog bort UseBrowerLink i startup

--- HotelsApi/HotelsApi/Startup.cs
+++ HotelsApi/HotelsApi/Startup.cs
@@ -35,15 +35,15 @@ namespace HotelsApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("Error");
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseBrowserLink();
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("Error");
+            //}
 
             app.UseStaticFiles();
             app.UseStatusCodePages();
