dd604bbac804144493ca41454b7dec2e00a81818
Amanda Westerlund <Westerlund922@gmail.com>
2018-05-15 11:08:52 +0200

    pillat med testet som fortfarande inte funkar.

--- HotelsApi/HotelsApiTest/UnitTest1.cs
+++ HotelsApi/HotelsApiTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using HotelsApi.Infrastructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,18 +10,25 @@ namespace HotelsApiTest
     public class UnitTest1
     {
         private FileReader fille = new FileReader();
+
         [TestInitialize]
         public void Init()
         {
             File.Create($@"wwwroot\Hotels\Scandic-{DateTime.Now:yyyy-MM-dd}.txt");
             File.WriteAllText($@"wwwroot\Hotels\Scandic-{DateTime.Now:yyyy-MM-dd}.txt", "50,Scandic Rubinen,15");
         }
+
         [TestMethod]
         public void TestScandicRubinen()
         {
-            Assert.AreEqual(50, fille.ReadScandicFile()[0].RegionValue);
-            Assert.AreEqual("Scandic Rubinen", fille.ReadScandicFile()[0].Name);
-            Assert.AreEqual(15, fille.ReadScandicFile()[0].RoomsAvailable);
+            var hotelList = fille.ReadScandicFile();
+            var hotel = hotelList[0];
+            Assert.AreEqual(50, hotel.RegionValue);
+            Assert.AreEqual("Scandic Rubinen", hotel.Name);
+            Assert.AreEqual(15, hotel.RoomsAvailable);
+            //Assert.AreEqual(50, fille.ReadScandicFile()[0].RegionValue);
+            //Assert.AreEqual("Scandic Rubinen", fille.ReadScandicFile()[0].Name);
+            //Assert.AreEqual(15, fille.ReadScandicFile()[0].RoomsAvailable);
         }
     }
 }
